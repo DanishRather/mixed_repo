7dc07f8fcd505b598b8a0ec74c1a63ade2a66197
DanishRather <tabnoone@gmail.com>
2025-09-18 15:39:14 +0530

    First Commiit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="hubspot-connector",
-    version="0.0.0",
+    version="0.0.1",
     author="dan ish",
-    author_email="dan@gmail.com",
+    author_email="dan @gmail.com",
     description="A utility class to interact with Hubspot using OAuth 2.0 credentials.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    py_modules=["hub_spot"],
+    packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[
         "requests"
